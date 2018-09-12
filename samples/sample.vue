21693c169e956879236f2e7414945ef71735d422
agorischek <alex.gorischek@gmail.com>
2018-09-11 20:15:21 -0700

    Add sample docs

---
+++ samples/sample.vue
@@ -0,0 +1,20 @@
+<template lang="louk">
+
+	//Comment here!
+	Logo
+	h1 name
+	h2 tagline
+	<div> Hey! </div>
+	~type coolClass
+	@click stretch
+	ul
+	#nav
+		li
+		-for link in links
+			a link.display
+			:href link.location
+	div instructions
+	.console
+	Sample
+
+</template>
