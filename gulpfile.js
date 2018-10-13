69fc420b25083e015c46f1e814d33c76a8d64151
agorischek <alex.gorischek@gmail.com>
2018-10-13 10:45:22 -0700

    Move most build functions to pipeline file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,9 @@
 var gulp = require('gulp');
-var multigrain = require('multigrain');
-var fs = require('fs');
 var pipeline = require('./pipeline/index.js');
-
-var grammarInput = "source/louk.YAML-tmLanguage";
-var settingsInput = "source/settings.yaml";
-var editors = multigrain.parse(fs.readFileSync("./source/editors.toml", "utf8"), "toml");
-var packages = multigrain.parse(fs.readFileSync("./source/packages.yaml", "utf8"), "yaml");
-var readmes = fs.readFileSync("./source/READMES.md", "utf8");
+var editors = pipeline.editors;
 
 gulp.task('build', function(done) {
-    build();
+    pipeline.build();
     done();
 });
 
@@ -35,77 +28,3 @@ gulp.task('default', gulp.series('build', 'preview', 'distribute'));
 gulp.task('watch', function() {
     return gulp.watch('/*', gulp.series('build', 'preview', 'distribute'));
 });
-
-function build(){
-
-    var grammar = parseGrammar(fs.readFileSync(grammarInput, "utf8"));
-    var settings = parseSettings(fs.readFileSync(settingsInput, "utf8"));
-
-    for(var editor in editors){
-      writeGrammar(editor, grammar);
-      writeSettings(editor, settings);
-      writePackageInfo(editor);
-      writeReadme(editor, readmes);
-    }
-
-}
-
-function writeGrammar(editor, grammar){
-
-    var info = editors[editor];
-    var dir = "staging/" + editor + "/" + info.grammarSubdir;
-
-    ensureDir(dir);
-
-    fs.writeFileSync(dir + info.grammarFile, grammar[info.format]);
-
-}
-
-function writeSettings(editor, settings){
-
-    var info = editors[editor];
-    var dir = "staging/" + editor + "/" + info.settingsSubdir;
-
-    if(info.settingsFile){
-        ensureDir(dir);
-        fs.writeFileSync(dir + info.settingsFile, settings[info.format]);
-    }
-}
-
-function writePackageInfo(editor){
-
-    var distFile = "staging/" + editor + "/package.json";
-    var packageInfo = multigrain.json(pipeline.package(packages, editor));
-    fs.writeFileSync(distFile, packageInfo);
-
-}
-
-function writeReadme(editor, content){
-
-    var distFile  = "staging/" + editor + "/README.md";
-    var readme = pipeline.readme(content, editor);
-    fs.writeFileSync(distFile , readme);
-
-}
-
-function parseGrammar(grammar){
-    var parsedGrammar = {};
-    parsedGrammar.yaml = grammar;
-    parsedGrammar.json = multigrain.json(parsedGrammar.yaml, "yaml");
-    parsedGrammar.cson = multigrain.cson(parsedGrammar.json, "json");
-    parsedGrammar.plist = multigrain.plist(parsedGrammar.json, "json");
-    return parsedGrammar;
-}
-
-function parseSettings(settings){
-    var parsedSettings = {};
-    parsedSettings.yaml = settings;
-    parsedSettings.cson = multigrain.cson(parsedSettings.yaml, "yaml");
-    return parsedSettings;
-}
-
-function ensureDir(dir){
-    if(!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-    }
-}
--- pipeline/index.js
+++ pipeline/index.js
@@ -1,8 +1,16 @@
 var merge = require('merge');
+var fs = require('fs');
+var multigrain = require('multigrain');
+
+var grammarInput = "source/louk.YAML-tmLanguage";
+var settingsInput = "source/settings.yaml";
+var editors = multigrain.parse(fs.readFileSync("./source/editors.toml", "utf8"), "toml");
+var packages = multigrain.parse(fs.readFileSync("./source/packages.yaml", "utf8"), "yaml");
+var readmes = fs.readFileSync("./source/READMES.md", "utf8");
 
 module.exports = {
-    readme: buildReadme,
-    package: buildPackage
+    build: build,
+    editors: editors
 };
 
 function buildPackage(packages, editor){
@@ -23,6 +31,7 @@ function buildReadme(content, editor){
     var general = "*";
 
     var sections = [];
+    var section = [];
 
     var appliesTo = [];
 
@@ -67,4 +76,78 @@ function buildReadme(content, editor){
     }
 
     return output;
+}
+
+function build(){
+
+    var grammar = parseGrammar(fs.readFileSync(grammarInput, "utf8"));
+    var settings = parseSettings(fs.readFileSync(settingsInput, "utf8"));
+
+    for(var editor in editors){
+      writeGrammar(editor, grammar);
+      writeSettings(editor, settings);
+      writePackageInfo(editor);
+      writeReadme(editor, readmes);
+    }
+
+}
+
+function writeGrammar(editor, grammar){
+
+    var info = editors[editor];
+    var dir = "staging/" + editor + "/" + info.grammarSubdir;
+
+    ensureDir(dir);
+
+    fs.writeFileSync(dir + info.grammarFile, grammar[info.format]);
+
+}
+
+function writeSettings(editor, settings){
+
+    var info = editors[editor];
+    var dir = "staging/" + editor + "/" + info.settingsSubdir;
+
+    if(info.settingsFile){
+        ensureDir(dir);
+        fs.writeFileSync(dir + info.settingsFile, settings[info.format]);
+    }
+}
+
+function writePackageInfo(editor){
+
+    var distFile = "staging/" + editor + "/package.json";
+    var packageInfo = multigrain.json(buildPackage(packages, editor));
+    fs.writeFileSync(distFile, packageInfo);
+
+}
+
+function writeReadme(editor, content){
+
+    var distFile  = "staging/" + editor + "/README.md";
+    var readme = buildReadme(content, editor);
+    fs.writeFileSync(distFile , readme);
+
+}
+
+function parseGrammar(grammar){
+    var parsedGrammar = {};
+    parsedGrammar.yaml = grammar;
+    parsedGrammar.json = multigrain.json(parsedGrammar.yaml, "yaml");
+    parsedGrammar.cson = multigrain.cson(parsedGrammar.json, "json");
+    parsedGrammar.plist = multigrain.plist(parsedGrammar.json, "json");
+    return parsedGrammar;
+}
+
+function parseSettings(settings){
+    var parsedSettings = {};
+    parsedSettings.yaml = settings;
+    parsedSettings.cson = multigrain.cson(parsedSettings.yaml, "yaml");
+    return parsedSettings;
+}
+
+function ensureDir(dir){
+    if(!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
 }
